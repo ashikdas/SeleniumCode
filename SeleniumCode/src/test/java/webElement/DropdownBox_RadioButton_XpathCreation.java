d40bc8618c18a363c9b959db91a9c6129801a6ea
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-09-27 15:40:14 +0600

    dropdown

--- SeleniumCode/src/test/java/webElement/DropdownBox_RadioButton_XpathCreation.java
+++ SeleniumCode/src/test/java/webElement/DropdownBox_RadioButton_XpathCreation.java
@@ -2,6 +2,7 @@ package webElement;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
@@ -14,26 +15,50 @@ public class DropdownBox_RadioButton_XpathCreation {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 				
-		//URL
-		driver.get("https://www.nexchar.com/signup");
+//		//URL
+//		driver.get("https://www.nexchar.com/signup");
+//		
+//		// -- Dropdownbox Testing --
+//		//select method creation
+//		
+//		//Select class object create named as "dropdownbox"
+//		Select dropdownbox = new Select(driver.findElement(By.id("CategoryId")));
+//		
+//		Thread.sleep(3000);
+//		//way 1
+//		dropdownbox.selectByVisibleText("Groceries & Food Retail");
+//		
+//		Thread.sleep(3000);
+//		//way 2
+//		dropdownbox.selectByValue("10");
+//		
+//		Thread.sleep(3000);
+//		//way 3
+//		dropdownbox.selectByIndex(4);
 		
-		// -- Dropdownbox Testing --
-		//select method creation
+		// ---Radio Button Test ----
+//		driver.get("http://demo.guru99.com/test/radio.html");
+//		
+//		driver.findElement(By.id("vfb-7-1")).click();
 		
-		//Select class object create named as "dropdownbox"
-		Select dropdownbox = new Select(driver.findElement(By.id("CategoryId")));
+		driver.get("https://www.nexchar.com/contactus");
 		
-		Thread.sleep(3000);
-		//way 1
-		dropdownbox.selectByVisibleText("Groceries & Food Retail");
+		// click on the input
+		WebElement Namebox = driver.findElement(By.id("FullName"));
 		
-		Thread.sleep(3000);
-		//way 2
-		dropdownbox.selectByValue("10");
 		
-		Thread.sleep(3000);
-		//way 3
-		dropdownbox.selectByIndex(4);
+		Namebox.click();
+		
+		Thread.sleep(2000);
+		//send some data into the input box
+		//driver.findElement(By.id("FullName")).sendKeys("OK");
+		Namebox.sendKeys("OK");
+		
+		Thread.sleep(2000);
+		//send some data into the input box
+		Namebox.clear();
+		
+		Thread.sleep(2000);
 				
 	}
 
